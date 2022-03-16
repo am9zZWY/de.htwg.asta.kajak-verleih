82fa1c430147c9797d15c792f11f10c309e8b5f4
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-17 00:27:48 +0100

    index.php: added check if it is a post request and started working on post evaluation.

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/scripts/database.php';
+require_once __DIR__ . '/scripts/helpers.php';
+
+$database = connect_to_database();
 ?>
 
-<html lang="de">
-<html xmlns="http://www.w3.org/1999/html">
+<html lang="de" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,36 +27,47 @@ require_once __DIR__ . '/scripts/database.php';
     Kajak Verleihsystem
 </h1>
 <?php
-// all weekdays in german
-$weekdays = array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag");
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = clean_string($_POST['name']);
+    $email = clean_string($_POST['email']);
+    $mobile = clean_string($_POST['mobile']);
+    $date = clean_string($_POST['date']);
+    $timeslots = clean_array($_POST['timeslots']);
 
-// start date is today
-$date = date_create();
+    echo $date;
+    var_dump($timeslots);
+} else {
+    // configs
+    // all weekdays in german
+    $weekdays = array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag");
+    // start date is today
+    $date = date_create();
+    // max days for calendar
+    $max_days = 14;
+    // timeslots
+    $timeslots = array("9:00 - 13:00", "13:00 - 18:00");
+    ?>
+    <div>
+        <form action="index.php" method="post">
+            <!-- select dates -->
+            <label for="date">Startdatum:</label>
+            <select id="date" name="date">
+                <?php for ($day = 0; $day < $max_days; $day++) { ?>
+                    <option value="<?php echo $date->format('d-m-Y') ?>">
+                        <?php echo $date->format('d-m-Y') ?>
+                    </option>
+                    <?php date_add($date, new DateInterval('P1D')); ?>
+                <?php } ?>
+            </select>
 
-$max_days = 14;
-?>
-<div>
-    <form action="index.php">
-        <!-- select dates -->
-        <label for="start_date">Startdatum:</label>
-        <select id="start_date">
-            <?php for ($day = 0; $day < $max_days; $day++) { ?>
-                <option value="<?php echo $day ?>">
-                    <?php echo $date->format('d-m-Y') ?>
-                </option>
-                <?php date_add($date, new DateInterval('P1D')); ?>
+            <!-- time slots -->
+            <?php foreach ($timeslots as $timeslot) { ?>
+                <input type="checkbox" id="<?php echo "time_" . $timeslot ?>" name="timeslots[]" value="<?php echo $timeslot ?>">
+                <label for="<?php echo "time_" . $timeslot ?>"><?php echo $timeslot ?></label>
             <?php } ?>
-        </select>
 
-        <!-- time slot-->
-        <label for="time">Zeitraum:</label>
-        <input type="checkbox" id="time">9:00 - 13:00</input>
-        <input type="checkbox" id="time">13:15 - 18:00</input>
-
-        <input type="submit" value="Anfrage senden"/>
-        <?php
-        connect_to_database();
-        ?>
-    </form>
-</div>
+            <input type="submit" value="Anfrage senden"/>
+        </form>
+    </div>
+<?php } ?>
 </html>
