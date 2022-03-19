abfb792e4e28af6f4e4a81fd90617f5e0c06636d
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-20 00:01:42 +0100

    index.php: improved style and extended form.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $database = connect_to_database();
     <title>Kajak Verleihsystem von Coolen Typen</title>
 </head>
 <body>
-<div id="booking" class="section">
+<div class="section">
     <div class="section-center">
         <div class="booking-cta">
             <a href="index.php" class="link-primary text-decoration-none">
@@ -29,16 +29,7 @@ $database = connect_to_database();
             <div class="row">
                 <?php
                 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                    $name = clean_string($_POST['name']);
-                    $email = clean_string($_POST['email']);
-                    $mobile = clean_string($_POST['mobile']);
-                    $date = clean_string($_POST['date']);
-                    $timeslots = clean_array($_POST['timeslots']);
-
-                    $connection = connect_to_database();
-                    prepare_reservation_table($connection);
-                    insert_reservation($connection, $name, $email, $mobile, $date, $timeslots);
-
+                    var_dump($_POST);
                 } else {
                 // configs
                 // all weekdays in german
@@ -57,73 +48,106 @@ $database = connect_to_database();
                 <div class="col-md-6">
                     <div class="booking-form">
                         <form action="index.php" method="post" class="needs-validation">
-                            <div class="form-group my-2">
-                                <label class="form-label">
+                            <div class="form-floating mb-3">
+                                <input name="name" type="text" placeholder="Max Musterfrau" id="name"
+                                       class="form-control"
+                                       required>
+                                <label for="name">
                                     Name
-                                    <input name="name" type="text" placeholder="Max Musterfrau"
-                                           class="form-control"
-                                           required>
                                 </label>
                             </div>
 
                             <div class="row my-2">
                                 <div class="col-sm-6">
-                                    <label class="form-label">
-                                        E-Mail
+                                    <div class="mb-3 form-floating">
                                         <input name="email" type="email" placeholder="ma391mus@htwg-konstanz.de"
+                                               id="email"
                                                class="form-control"
                                                required>
-                                    </label>
+                                        <label for="email">
+                                            E-Mail
+                                        </label>
+                                    </div>
                                 </div>
 
                                 <div class="col-sm-6">
-                                    <label class="form-label">
-                                        Telefonnummer
-                                        <input name="mobile" type="tel" placeholder="+49 (0) 123 456789"
+                                    <div class="mb-3 form-floating">
+                                        <input name="phone" type="tel" placeholder="+49 (0) 123 456789" id="phone"
                                                class="form-control"
                                                required>
-                                    </label>
+                                        <label for="phone">
+                                            Telefonnummer
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
 
                             <div class="row my-2">
                                 <div class="col-sm-6">
                                     <!-- select dates -->
-                                    <div class="form-group">
-                                        <label class="form-label">
+                                    <div class="form-group form-floating">
+                                        <select name="date" class="form-select" id="date" autocomplete="on" required>
+                                            <?php for ($day = 0; $day < $max_days; $day++) {
+                                                $weekday = (int)$date->format('w');
+                                                if ($weekday !== 0 && $weekday !== 6) { ?>
+                                                    <option value="<?php echo $date->format('Y-m-d') ?>">
+                                                        <?php echo $weekdays[$weekday] . ' ' . $date->format('d.m.Y') ?>
+                                                    </option>
+                                                    <?php
+                                                }
+                                                date_add($date, new DateInterval('P1D'));
+                                            } ?>
+                                        </select>
+                                        <label for="date">
                                             Datum
-                                            <select name="date" class="form-select" required>
-                                                <?php for ($day = 0; $day < $max_days; $day++) {
-                                                    $weekday = (int)$date->format('w');
-                                                    if ($weekday !== 0 && $weekday !== 6) { ?>
-                                                        <option value="<?php echo $date->format('l d.m.Y') ?>">
-                                                            <?php echo $weekdays[$weekday] . ' ' . $date->format('d.m.Y') ?>
-                                                        </option>
-                                                        <?php
-                                                    }
-                                                    date_add($date, new DateInterval('P1D'));
-                                                } ?>
-                                            </select>
                                         </label>
                                     </div>
                                 </div>
 
                                 <div class="col-sm-6">
-                                    <b>Zeitslots</b><br>
-                                    <!-- time slots -->
-                                    <?php foreach ($timeslots as $timeslot) { ?>
-                                        <label class="form-check-label">
-                                            <input type="checkbox" name="timeslots[]"
-                                                   value="<?php echo $timeslot ?>"
-                                                   class="form-check-input">
-                                            <?php echo $timeslot ?>
-                                        </label><br>
-                                    <?php } ?>
+                                    <div class="form-group form-floating">
+                                        <span class="form-label">Zeitslots</span><br>
+                                        <!-- time slots -->
+                                        <?php foreach ($timeslots as $timeslot) { ?>
+                                            <label class="form-check-label">
+                                                <input type="checkbox" name="timeslots[]"
+                                                       value="<?php echo $timeslot ?>"
+                                                       class="form-check-input">
+                                                <?php echo $timeslot ?>
+                                            </label>
+                                            <br>
+                                        <?php } ?>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div class="col-sm-6">
-                                <input type="submit" value="Anfrage senden" class="btn btn-primary"/>
+                            <div class="row my-2">
+                                <div class="col-md-6">
+                                    <img alt="Bild eines einzelnen Kajaks" class="img-fluid"/>
+                                    <div class="form-group form-floating">
+                                        <input type="number" max="9" min="0" id="single-kajak" value="0"
+                                               name="single-kajak" class="form-control"/>
+                                        <label class="form-check-label" for="single-kajak">
+                                            Anzahl 1-Sitz Kajaks
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <img alt="Bild eines doppelten Kajaks" class="img-fluid"/>
+                                    <div class="form-group form-floating">
+                                        <input type="number" max="9" min="0" id="double-kajak" value="0"
+                                               name="double-kajak" class="form-control"/>
+                                        <label class="form-check-label" for="double-kajak">
+                                            Anzahl 2-Sitz Kajaks
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                    <input type="submit" value="Anfrage senden" class="btn btn-primary"/>
+                                </div>
                             </div>
                         </form>
                     </div>
