41c1ab63141168ac921d73770e620d8791095d55
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-26 12:52:36 +0100

    script_template_helpers.php: added fct to create accordion.

--- scripts/script_template_helpers.php
+++ scripts/script_template_helpers.php
@@ -24,3 +24,28 @@ function create_header(string $header, string $link = '', bool $echo = true): st
     }
     return $created_header;
 }
+
+/**
+ * Creates an accordion.
+ *
+ * @param bool $echo
+ * @return string
+ */
+function create_accordion(bool $echo = true): string
+{
+    $created_accordion = "<script>
+                const acc = document.getElementsByClassName('accordion');
+                Array.from(acc).forEach((item) => {
+                    item.addEventListener('click', function () {
+                        item.classList.toggle('active');
+                        const panel = item.nextElementSibling;
+                        panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + 'px';
+                    });
+                })
+            </script>";
+    
+    if ($echo) {
+        echo $created_accordion;
+    }
+    return $created_accordion;
+}
