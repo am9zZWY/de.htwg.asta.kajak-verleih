708a3b54bbb5d0c90cb9b4d89c6aef5f72bcc7a1
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-26 12:34:23 +0100

    about.php: removed repetitive code.

--- pages/user/about.php
+++ pages/user/about.php
@@ -1,49 +1,33 @@
 <?php
-include '../../templates/head.php'
+create_header('Informationen zur Reservierung');
 ?>
-<body>
-<?php include '../../templates/sidebar.php' ?>
-
-<div class="bg">
-    <div class="section" id="booking">
-    <div class="section-center">
-        <div class="booking-cta">
-            <div class="container">
-                <div class="row">
-                    <h2 class="primary">Info </h2>
-                </div>
-                <div class="container" style="border: white solid;">
-                    <div class="row">
-                        <h3 class="primary">Mietsachen</3>
-                    </div>
-                    <p>Vermietet werden Kajaks, mit einem Mietvertrag festgehalten Ausrüstungsgegenstände.
-                        Diese werden für einen sicheren Betrieb der Kajaks benötigt.
-                    </p>
-                    <div class="row">
-                        <button class="accordion">Haftung</button>
-                        <div class="panel">
-                            <p>Lorem ipsum..</p>
-                        </div>
-                        <button class="accordion">Rechtliches</button>
-                        <div class="panel">
-                            <p>test ipsum..</p>
-                        </div>
-                        <script>
-                            const acc = document.getElementsByClassName("accordion");
-                            Array.from(acc).forEach((item) => {
-                                item.addEventListener("click", function () {
-                                    item.classList.toggle("active");
-                                    const panel = item.nextElementSibling;
-                                    panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + "px";
-                                });
-                            })
-                        </script>
-                    </div>
-                </div>
+<div class="container-fluid">
+    <div class="container" style="border: white solid;">
+        <div class="row">
+            <h3 class="primary">Mietsachen</3>
+        </div>
+        <p>Vermietet werden Kajaks, mit einem Mietvertrag festgehalten Ausrüstungsgegenstände.
+            Diese werden für einen sicheren Betrieb der Kajaks benötigt.
+        </p>
+        <div class="row">
+            <button class="accordion">Haftung</button>
+            <div class="panel">
+                <p>Lorem ipsum..</p>
             </div>
+            <button class="accordion">Rechtliches</button>
+            <div class="panel">
+                <p>test ipsum..</p>
+            </div>
+            <script>
+                const acc = document.getElementsByClassName("accordion");
+                Array.from(acc).forEach((item) => {
+                    item.addEventListener("click", function () {
+                        item.classList.toggle("active");
+                        const panel = item.nextElementSibling;
+                        panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + "px";
+                    });
+                })
+            </script>
         </div>
     </div>
-    </div>
 </div>
-</body>
-</html>
