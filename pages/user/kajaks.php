5631ec663479af6e8b774a4f3c270d37f8212dc7
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-26 12:34:51 +0100

    kajaks.php: removed repetitive code and fixed ui.

--- pages/user/kajaks.php
+++ pages/user/kajaks.php
@@ -1,32 +1,15 @@
 <?php
-include '../../templates/head.php'
+create_header('Kajaks');
 ?>
-<body>
-<?php include '../../templates/sidebar.php' ?>
-
-<div class="bg">
-    <div class="section">
-    <div class="section-center">
-        <div class="booking-cta">
-            <div class="container">
-                <div class="row">
-                    <h2 class="primary">Kajaks </h2>
-                </div>
-                <div class="row">
-                    <div class="booking-cta">
-                        <div class="col-md-6">
-                            <h3 class="primary">Hi</3>
-                        </div>
-                        <div class="col-md-6">
-                            <h3>Bye</h3>
-                         </div>
-                    </div>
-                </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="header-wrapper">
+            <div class="col-md-6">
+                <h3 class="primary">Hi</3>
+            </div>
+            <div class="col-md-6">
+                <h3>Bye</h3>
             </div>
         </div>
     </div>
-    </div>
 </div>
-
-</body>
-</html>
