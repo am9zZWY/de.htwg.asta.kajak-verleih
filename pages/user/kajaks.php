5446ea1559a19b6c243ffe2797c1d560bc71ddf1
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-26 12:58:47 +0100

    kajaks.php: fixed path to images.

--- pages/user/kajaks.php
+++ pages/user/kajaks.php
@@ -4,10 +4,12 @@ create_header('Kajaks');
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-6">
-            <img src="resources/images/nature-canoes-mountains-water-wallpaper-preview.jpg" width="300" height="200">
+            <img alt="Bild eines einzelnen Kajaks" src="/resources/images/einzelKajak.png"
+                 class="img-fluid" style="width: 300px; height: 200px;"/>
         </div>
         <div class="col-md-6">
-            <img src="resources/images/nature-canoes-mountains-water-wallpaper-preview.jpg" width="300" height="200">
+            <img alt="Bild eines doppelten Kajaks" src="/resources/images/doppelKajak.png"
+                 class="img-fluid" style="width: 300px; height: 200px;"/>
         </div>
     </div>
 </div>
