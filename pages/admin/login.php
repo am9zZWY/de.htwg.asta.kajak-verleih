bf5c6708f3667b1e5320a61b50dbf680040f33b4
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-26 12:35:00 +0100

    login.php: removed repetitive code and fixed ui.

--- pages/admin/login.php
+++ pages/admin/login.php
@@ -1,62 +1,50 @@
 <?php
-include '../../templates/head.php';
-require __DIR__ . '/../../scripts/login.php';
-require __DIR__ . '/../../scripts/helpers.php';
-
+create_header('Login');
 ?>
-<html lang="de" xmlns="http://www.w3.org/1999/html">
-<body>
-<?php include '../../templates/sidebar.php' ?>
-<div class="section" id="booking">
-    <div class="container-fluid">
-        <div class="row align-items-center">
-            <h1 class="primary">Are u even Admin?</h1>
-        </div>
-
-        <div class="row align-items-center">
-            <div class="col m-auto">
-                <form class="animate" method="post">
-                    <div class="imgcontainer">
-                        <img src="/resources/images/login.PNG" alt="Avatar" class="avatar">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-sm-6">
+            <div class="custom-form">
+                <form class="animate" method="post" class="needs-validation">
+                    <div class="img-container">
+                        <img src="/resources/images/login.PNG" alt="Avatar" class="img-avatar">
                     </div>
 
-                    <div class="container">
-                        <div class="form-floating mb-3">
-                            <input name="name" type="text" id="name"
-                                   class="form-control"
-                                   required>
-                            <label for="name">
-                                Name
-                            </label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input name="password" type="password" id="password" class="form-control" required/>
-                            <label for="password">
-                                Password
-                            </label>
-                        </div>
-                        <button type="submit">Login</button>
-                        <?php
-                        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                            $logged_in = login(clean_string($_POST['name']), clean_string($_POST['password']));
-                            if (!$logged_in) {
-                                ?>
-                                <h3>Einloggen fehlgeschlagen</h3>
-                                <?php
-                            } else {
-                                ?>
+                    <div class="form-floating mb-3">
+                        <input name="name" type="text" placeholder="Max Musterfrau" id="name"
+                               class="form-control"
+                               required>
+                        <label for="name">
+                            Name
+                        </label>
+                    </div>
 
-                                <h3>Einloggen erfolgreich</h3>
-                                <?php
-                            }
+                    <div class="form-floating mb-3">
+                        <input name="password" type="password" placeholder="123" id="password"
+                               class="form-control"
+                               required>
+                        <label for="password">
+                            Passwort
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary submit-btn">Login</button>
+                    <?php
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                        $logged_in = login(clean_string($_POST['name']), clean_string($_POST['password']));
+                        if (!$logged_in) {
+                            ?>
+                            <h3>Einloggen fehlgeschlagen</h3>
+                            <?php
+                        } else {
+                            ?>
+
+                            <h3>Einloggen erfolgreich</h3>
+                            <?php
                         }
-                        ?>
-                    </div>
+                    }
+                    ?>
                 </form>
             </div>
         </div>
     </div>
 </div>
-
-</body>
-</html>
