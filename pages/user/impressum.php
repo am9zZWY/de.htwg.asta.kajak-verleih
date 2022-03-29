db4df80475e78000b55c6e7a7718dd1cbdb7a2ff
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-29 19:18:16 +0200

    :lipstick: impressum.php: improved style.

--- pages/user/impressum.php
+++ pages/user/impressum.php
@@ -1,20 +1,16 @@
 <?php
 create_header('Impressum')
 ?>
-<div class="container-fluid">
-    <div class="container" style="border: white solid;">
-        <div class="row">
-            <h3 class="primary">AStA der Hochschule Konstanz</3>
-        </div>
-        <p>
-            <b>Hochschule Konstanz</b>
-            <br>
-            Alfred-Wachtelstraße 8 <br>
-            78462 Konstanz<br>
-            Gebäude D<br><br><br>
-            <b>Kontaktaufnahme</b><br>
-            Tel.: 07531 206-431/-122<br>
-            E-mail: asta@htwg-konstanz.de
-        </p>
+<div class="container container-content rounded">
+    <div>
+        <h3>AStA</h3>
+        <b>Hochschule Konstanz</b>
+        <br>
+        Alfred-Wachtelstraße 8 <br>
+        78462 Konstanz<br>
+        Gebäude D<br><br><br>
+        <b>Kontaktaufnahme</b><br>
+        Tel.: 07531 206-431/-122<br>
+        E-Mail: asta@htwg-konstanz.de
     </div>
 </div>
