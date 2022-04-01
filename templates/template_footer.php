d14e18eef02f9c2decddd1c6777ef14356cd4651
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-01 23:46:08 +0200

    template_footer.php: small fix.

--- templates/template_footer.php
+++ templates/template_footer.php
@@ -1,6 +1,6 @@
 <footer class="text-center text-lg-start bg-light text-muted">
     <strong>
-        Copyright &copy; 2022 Vasilij Slabinski & Josef Müller</a>.
+        Copyright &copy; 2022 Vasilij Slabinski & Josef Müller.
     </strong>
     Alle Rechte vorbehalten.
 </footer>
