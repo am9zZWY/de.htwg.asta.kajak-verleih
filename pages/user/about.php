850976755549cbc282f984e13f0018c2c0be92d0
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-01 02:18:37 +0200

    :art: about.php: refined code.

--- pages/user/about.php
+++ pages/user/about.php
@@ -2,59 +2,86 @@
 create_header('Informationen zur Reservierung');
 ?>
 <div class="container-fluid">
-    <div class="container" style="border: white solid;">
+    <div class="container">
         <div class="row">
-            <h3 class="primary">Mietsachen</h3>
-
-            <div class="header-wrapper" style="text-align: justify;">
-            <br><p>Vermietet werden Kajaks, mit einem Mietvertrag festgehalten Ausrüstungsgegenstände.
-            Diese werden für einen sicheren Betrieb der Kajaks benötigt.
-            Der Mieter verpflichtet sich, die Mietgegenstände mit Sorgfalt zu behandeln. Schäden und Verschmutzungen werden auf Kosten des Mieters entfernt. Zum festgelegten Abhol-/Rückholtermin hat der Mieter die Mietgegenstände vollständig und persönlich dem Vermieter zurückzugeben. Fehlende oder kaputte Gegenstände werden zum Wiederbeschaffungswert berechnet.
-            </p>
+            <div class="content">
+                <div>
+                    <h3>Mietsachen</h3>
+                    <p>Vermietet werden Kajaks, mit einem Mietvertrag festgehalten Ausrüstungsgegenstände.
+                        Diese werden für einen sicheren Betrieb der Kajaks benötigt.
+                        Der Mieter verpflichtet sich, die Mietgegenstände mit Sorgfalt zu behandeln. Schäden und
+                        Verschmutzungen werden auf Kosten des Mieters entfernt. Zum festgelegten Abhol-/Rückholtermin
+                        hat
+                        der Mieter die Mietgegenstände vollständig und persönlich dem Vermieter zurückzugeben. Fehlende
+                        oder
+                        kaputte Gegenstände werden zum Wiederbeschaffungswert berechnet.
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="row"style="text-align: justify;">
-            <button class="accordion"><b>Haftung</b></button>
+        <div class="row" style="text-align: justify;">
+            <button class="accordion"><strong>Haftung</strong></button>
             <div class="panel">
                 <p><h5>Haftungsausschluss</h5>
 
-                    <b>Der AStA der HTWG Konstanz, haftet als reine Vemietung nicht für Personen-, Sach- und Transportschäden.</b><br>
-                   <br> Für verlorene oder beschädigte Wertsachen, Ausweise, Handys, Kameras etc. wird keinerlei Haftung übernommen.
-                    Der Vermieter haftet weder vertraglich noch außervertraglich für irgendwelche Schäden, die dem Benutzer oder Dritten im Zusammenhang mit der Überlassung der Mietgegenstände entstehen.
-                    Der Benutzer wird den Vermieter von Ansprüchen Dritter aufgrund von Unfällen freistellen.
-                    Der Benutzer stellt den Vermieter von sämtlichen Ansprüchen aus der Verletzung von Gesetzen, Verordnungen oder sonstigen Vorschriften in Zusammenhang mit dem Gebrauch der Mietgegenstände durch ihn oder eine dritte Person frei.
+                <strong>Der AStA der HTWG Konstanz, haftet als reine Vemietung nicht für Personen-, Sach- und
+                    Transportschäden.</strong><br>
+                <br> Für verlorene oder beschädigte Wertsachen, Ausweise, Handys, Kameras etc. wird keinerlei Haftung
+                übernommen.
+                Der Vermieter haftet weder vertraglich noch außervertraglich für irgendwelche Schäden, die dem Benutzer
+                oder Dritten im Zusammenhang mit der Überlassung der Mietgegenstände entstehen.
+                Der Benutzer wird den Vermieter von Ansprüchen Dritter aufgrund von Unfällen freistellen.
+                Der Benutzer stellt den Vermieter von sämtlichen Ansprüchen aus der Verletzung von Gesetzen,
+                Verordnungen oder sonstigen Vorschriften in Zusammenhang mit dem Gebrauch der Mietgegenstände durch ihn
+                oder eine dritte Person frei.
                 <br><br>
-                <b>Wir weisenausdrücklich darauf hin, dass viele Flüsse, Seen, Bachläufe und Uferzonen unter Naturschutz stehen und aufgrund dessen mit großer Rücksichtnahme zu agieren ist!</b>
-                In unmittelbahrer Ufernähe sollte nicht gepaddelt werden, da durch liegengebliebene Äste, niedrigen Wasserstand und Gegenstände Gefahren bergen!
+                <strong>Wir weisenausdrücklich darauf hin, dass viele Flüsse, Seen, Bachläufe und Uferzonen unter
+                    Naturschutz
+                    stehen und aufgrund dessen mit großer Rücksichtnahme zu agieren ist!</strong>
+                In unmittelbahrer Ufernähe sollte nicht gepaddelt werden, da durch liegengebliebene Äste, niedrigen
+                Wasserstand und Gegenstände Gefahren bergen!
                 </p>
             </div>
-            <button class="accordion"><b>Rechtliches</b></button>
+            <button class="accordion"><strong>Rechtliches</strong></button>
             <div class="panel">
                 <p>test ipsum..</p>
             </div>
-            <button class="accordion"><b>Sicherheit</b></button>
+            <button class="accordion"><strong>Sicherheit</strong></button>
             <div class="panel">
-                <p> <b>Jeder in einem Kajak Fahrende muss schwimmen können und eine Schwimmweste tragen.</b>
-                    <br><br>Aus Sicherheitsgründen dürfen Kinder mit uns nur <b>ab dem 12. Lebensjahr</b> an Kajakfahrten teilnehmen.
-                      Die erwachsenen Aufsichtspersonen tragen die volle Verantwortung.
-                      Eigene Westen sind unerwünscht. Aufblasbare Schwimmhilfen sind nicht erlaubt.
-                      Bei Missachtung dieser Vorsichtsmaßnahmen sind wir berechtigt, die Tour sofort abzubrechen.
-                      </p>
+                <p><strong>Jeder in einem Kajak Fahrende muss schwimmen können und eine Schwimmweste tragen.</strong>
+                    <br><br>Aus Sicherheitsgründen dürfen Kinder mit uns nur <strong>ab dem 12. Lebensjahr</strong> an
+                    Kajakfahrten teilnehmen.
+                    Die erwachsenen Aufsichtspersonen tragen die volle Verantwortung.
+                    Eigene Westen sind unerwünscht. Aufblasbare Schwimmhilfen sind nicht erlaubt.
+                    Bei Missachtung dieser Vorsichtsmaßnahmen sind wir berechtigt, die Tour sofort abzubrechen.
+                </p>
             </div>
-            <button class="accordion"><b>Höhere Gewalt</b></button>
+            <button class="accordion"><strong>Höhere Gewalt</strong></button>
             <div class="panel">
-                <p><b>Der AStA der HTWG</b> ist bei einer Gesundheit gefährdenden Situation aufgrund der Wetterlage (Sturm, Unwetter, Überschwemmungen, extremes Niedrigwasser oder starker Nebel) berechtigt, <b>Touren abzusagen.</b>
-                    <br><br>In diesem Falle hat der Mieter <b>keinen Anspruch</b> auf Kostenerstattung von Nebenkosten (Anreise, Hotelübernachtung etc.).
-                    Dieses gilt auch für den Fall, dass die Tour aufgrund niedriger Wasserstände nicht stattfindet oder auf ein anderes Gewässer verlegt werden muss.</p>
+                <p><strong>Der AStA der HTWG</strong> ist bei einer Gesundheit gefährdenden Situation aufgrund der
+                    Wetterlage
+                    (Sturm, Unwetter, Überschwemmungen, extremes Niedrigwasser oder starker Nebel) berechtigt, <strong>Touren
+                        abzusagen.</strong>
+                    <br><br>In diesem Falle hat der Mieter <strong>keinen Anspruch</strong> auf Kostenerstattung von
+                    Nebenkosten
+                    (Anreise, Hotelübernachtung etc.).
+                    Dieses gilt auch für den Fall, dass die Tour aufgrund niedriger Wasserstände nicht stattfindet oder
+                    auf ein anderes Gewässer verlegt werden muss.</p>
             </div>
-            <button class="accordion"><b>Sorgfaltspflicht</b></button>
+            <button class="accordion"><strong>Sorgfaltspflicht</strong></button>
             <div class="panel">
-                <p><b>Fahrverbote und private Gebote in Bereichen nicht öffentlicher Grundstücke sind einzuhalten.</b>
-                    <br><br>Wiesen, Weiden und Felder an den Ufern sind in privatem Besitz und dürfen nicht betreten oder zur Rast genutzt werden.
-                    <b>Während der Fahrt dürfen die Kajaks nur an öffentlichen und besonders gekennzeichneten Rastplätzen verlassen werden.</b>
-                    Abfälle sind in mitzubringenden Mülltüten zu sammeln und, sofern vorhanden, am Ende der Tour in öffentlich aufgestellten Mülleimern zu entsorgen oder dem Hausmüll zuzuführen.
+                <p><strong>Fahrverbote und private Gebote in Bereichen nicht öffentlicher Grundstücke sind
+                        einzuhalten.</strong>
+                    <br><br>Wiesen, Weiden und Felder an den Ufern sind in privatem Besitz und dürfen nicht betreten
+                    oder zur Rast genutzt werden.
+                    <strong>Während der Fahrt dürfen die Kajaks nur an öffentlichen und besonders gekennzeichneten
+                        Rastplätzen verlassen werden.</strong>
+                    Abfälle sind in mitzubringenden Mülltüten zu sammeln und, sofern vorhanden, am Ende der Tour in
+                    öffentlich aufgestellten Mülleimern zu entsorgen oder dem Hausmüll zuzuführen.
                     Sämtliche Rastplätze sind absolut sauber zu hinterlassen.
-                    Bei Missachtung der Natur, grobem Fehlverhalten – auch durch Alkoholmissbrauch -, Lärm, Materialmissbrauch und unzulässiger Müllentsorgung kann der Vermieter den sofortigen Abbruch der Tour fordern.
+                    Bei Missachtung der Natur, grobem Fehlverhalten – auch durch Alkoholmissbrauch -, Lärm,
+                    Materialmissbrauch und unzulässiger Müllentsorgung kann der Vermieter den sofortigen Abbruch der
+                    Tour fordern.
                     Sämtliche Kosten und Folgekosten sind vom Auftragsgeber zu zahlen.</div>
             <?php echo create_accordion(false) ?>
         </div>
