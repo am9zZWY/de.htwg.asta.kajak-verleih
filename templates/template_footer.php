815fade0cbefca70180657108f8f17b7b9d404ad
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-01 23:16:34 +0200

    template_footer.php: added footer.

---
+++ templates/template_footer.php
@@ -0,0 +1,6 @@
+<footer class="text-center text-lg-start bg-light text-muted">
+    <strong>
+        Copyright &copy; 2022 Vasilij Slabinski & Josef Müller</a>.
+    </strong>
+    Alle Rechte vorbehalten.
+</footer>
