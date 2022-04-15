f8132d5d588aaca9dec6877f053657575f880e86
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-15 15:13:45 +0200

    script_config.php: added some doc fct to calculate price and removed some fcts.

--- scripts/script_config.php
+++ scripts/script_config.php
@@ -2,20 +2,13 @@
 
 class Config
 {
-    // construtor -> auslesen aus config.xml
-    // getDays -> liefert die Wochentage als Array
-    // getTimeslots -> liefert
-    // addTimeslot -> fügt ein neues Timeslot hinzu
-    // setTimeslot -> setzt ein Timeslot
-    // ...
-    // saveAsXML -> speichert die Konfiguration als XML in config.xml
-
-    private $config;
     private array $days;
     private array $timeslots;
     private SimpleXMLElement $xml;
 
-
+    /**
+     * Constructor for Config class
+     */
     public function __construct()
     {
         $loaded_xml = simplexml_load_string(file_get_contents("config.xml"));
@@ -25,6 +18,10 @@ class Config
         $this->xml = $loaded_xml;
     }
 
+    /**
+     * Get list of all kajaks
+     * @return array
+     */
     public function getKajaks()
     {
         $kajaks = array();
@@ -34,8 +31,6 @@ class Config
             $kajak->seats = (string)$child->seats;
             $kajak->amount = (string)$child->amount;
             $kajaks[] = $kajak;
-
-
         }
         return $kajaks;
     }
@@ -46,46 +41,54 @@ class Config
             "Sitze: " . $kajak->seats . "<br>" .
             "Anzahl: " . $kajak->amount . "<br>" .
             "------------------------------" . "<br>";
-
     }
 
-    public function getPrice(): array
+    public function getPrices(): array
     {
         $prices = array();
         foreach ($this->xml->prices->children() as $child) {
             $price = new stdClass();
             $price->name = (string)$child->attributes()->name;
             $price->price = (string)$child;
-            $prices[] =$price;
-
+            $prices[] = $price;
         }
         return $prices;
+    }
+
+    /**
+     * Calculate price depending on timeslots and amount of kajaks
+     * @param array $timeslots
+     * @param array $kajaks
+     * @return int
+     */
+    public function calculatePrice(array $timeslots, array $kajaks): int
+    {
+        /* prepare amount/factor */
+        $amount_kajaks = count($kajaks[0]) + count($kajaks[1]);
+
+        /* prepare prices */
+        $prices = $this->getPrices();
+        $price_single = $prices["single"];
+        $price_double = $prices["double"];
+        $price_kaution = $prices["kaution"];
+
+        /* if one timeslot */
+        if (count($timeslots) === 1) {
+            return ($amount_kajaks * $price_single) + $price_kaution;
+        }
+
+        /* if two timeslots */
+        if (count($timeslots) === 2) {
+            return ($amount_kajaks * $price_double) + $price_kaution;
+        }
+
+        return 0;
     }
 
     public function pricesToString($prices): string
     {
         return "Timeslot: " . $prices->name . "<br>" .
             "Preis: " . $prices->price . "<br>" .
-            "------------------------------" . "<br>";
-    }
-
-
-    public function getKaution(): array
-    {
-        $kaution = array();
-        foreach ($this->xml->kautionen->children() as $child) {
-            $kaution = new stdClass();
-            $kaution->name = (string)$child->attributes()->name;
-            $kaution->value= (string)$child;
-            $kautionen[] =$kaution;
-
-        }
-        return $kautionen;
-    }
-
-    public function kautionToString($kautionen): string
-    {
-        return "Kaution: " . $kautionen->value . "<br>" .
             "------------------------------" . "<br>";
     }
 
@@ -97,8 +100,7 @@ class Config
             $timeslot->name = (string)$child->attributes()->name;
             $timeslot->start = (string)$child->start;
             $timeslot->ende = (string)$child->ende;
-            $timeslots[] =$timeslot;
-
+            $timeslots[] = $timeslot;
         }
         return $timeslots;
     }
@@ -111,25 +113,35 @@ class Config
             "------------------------------" . "<br>";
     }
 
+    /**
+     * @param $timeslot
+     * @return void
+     * @deprecated
+     */
     public function addTimeslot($timeslot): void
     {
         $timeslot->setId(count($this->timeslots) + 1);
         $this->timeslots[] = $timeslot;
     }
 
+    /**
+     * @param $timeslot
+     * @return void
+     * @deprecated
+     */
     public function setTimeslot($timeslot): void
     {
         $this->timeslots[$timeslot->getId()] = $timeslot;
     }
 
-
-
-    public function formatTimeslot($formatTimeslot): void
+    /**
+     * Formats multiple timeslots from e.g. [9:00, 13:00] to 9:00 - 13:00
+     * @param $timeslots
+     * @return array
+     */
+    public function formatTimeslot($timeslots): array
     {
-        
-        $timeslots = array(array("09:00:00", "13:00:00"), array("13:00:00", "18:00:00"));
-
-        $timeslots_formatted = array_map(static function ($array) {
+        return array_map(static function ($array) {
             $timeslot = array_map(static function ($time) {
                 return date('H:i', strtotime($time));
             }, $array);
@@ -138,7 +150,10 @@ class Config
 
     }
 
-
+    /**
+     * Get all weekdays
+     * @return array
+     */
     public function getDays(): array
     {
         $days = array();
