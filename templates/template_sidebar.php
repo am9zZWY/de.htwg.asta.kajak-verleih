65964d2ccc1589ae2f75b593fa1ddb71ed64fb13
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-20 00:06:24 +0200

    template_sidebar.php: moved admin sidebar.

--- templates/template_sidebar.php
+++ templates/template_sidebar.php
@@ -1,10 +1,33 @@
-<!-- Menu Sidebar   -->
+<!-- Menu Sidebar -->
+<?php
+if (($_SERVER['REQUEST_METHOD'] === 'POST') && isset($_POST['logout'])) {
+    logout();
+    ?>
+    <script>
+        window.location.href = "/";
+    </script>
+    <?php
+}
+?>
+
 <div id="mySidenav" class="sidenav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <a href="/">Home</a>
     <a href="/kajaks">Kajaks</a>
     <a href="/about">Info</a>
     <a href="/impressum">Impressum</a>
+    <?php
+    if (is_logged_in()) {
+        ?>
+        <a href="/reservations">Reservierungen</a>
+        <a href="/how_to_admin">How To Admin</a>
+        <a href="/config">Konfiguration</a>
+        <form method="POST">
+            <button type="submit" name="logout">Abmelden</button>
+        </form>
+        <?php
+    }
+    ?>
 </div>
 <div id="burger" class="sidenav-burger">
     <span style="font-size:30px;background-color: transparent;cursor:pointer" onclick="openNav()">&#9776;</span>
@@ -21,5 +44,4 @@
         document.getElementById("burger").style.marginLeft = "0";
         document.body.style.backgroundColor = "white";
     }
-
 </script>
