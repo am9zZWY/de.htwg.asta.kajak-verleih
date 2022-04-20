a19635605e3a54aec577254b53ef38d678b5a523
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-21 01:05:13 +0200

    component_sidebar.php: renamed to component and changed some names.

--- components/component_sidebar.php
+++ components/component_sidebar.php
@@ -12,21 +12,20 @@ if (($_SERVER['REQUEST_METHOD'] === 'POST') && isset($_POST['logout'])) {
 
 <nav class="fixed-top">
     <div id="burger" class="sidenav-burger">
-        <span style="font-size:30px;background-color: transparent;cursor:pointer" onclick="openNav()">&#9776;</span>
+        <span onclick="openNav()">&#9776;</span>
     </div>
 </nav>
-<div id="mySidenav" class="sidenav">
+<div id="sidenav" class="sidenav">
     <a href="javascript:void(0)" class="close-btn" onclick="closeNav()">&times;</a>
-    <a href="/">Home</a>
-    <a href="/kajaks">Kajaks</a>
+    <a href="/">Startseite</a>
     <a href="/about">Info</a>
     <a href="/impressum">Impressum</a>
     <?php
     if (is_logged_in()) {
         ?>
         <a href="/reservations">Reservierungen</a>
-        <a href="/how_to_admin">How To Admin</a>
         <a href="/config">Konfiguration</a>
+        <a href="/how_to_admin">Anleitung</a>
         <form method="POST">
             <button type="submit" name="logout">Abmelden</button>
         </form>
@@ -36,13 +35,13 @@ if (($_SERVER['REQUEST_METHOD'] === 'POST') && isset($_POST['logout'])) {
 </div>
 <script lang="javascript">
     function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("sidenav").style.width = "250px";
         document.getElementById("burger").style.marginLeft = "250px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     }
 
     function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("sidenav").style.width = "0";
         document.getElementById("burger").style.marginLeft = "0";
         document.body.style.backgroundColor = "white";
     }
