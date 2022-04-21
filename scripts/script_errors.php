06b0aa7acc4f9d69ee7499732a31251d15bf480e
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-22 01:28:53 +0200

    :broom: script_errors.php: cleaned up.

--- scripts/script_errors.php
+++ scripts/script_errors.php
@@ -7,17 +7,19 @@ $ERROR_DATABASE_QUERY = "Fehler bei der Vorbereitung";
 
 /* Form reservation */
 $ERROR_RESERVATION_TIMESLOT_NOT_SELECTED = "Bitte wähle eine Zeit aus.";
-$ERROR_RESERVATION_KAJAK_TYPE_NOT_FOUND = "Kajaktyp nicht gefunden";
-$ERROR_RESERVATION_SINGLE_KAJAK_NOT_AVAILABLE = "Einzelkajak nicht verfügbar";
-$ERROR_RESERVATION_DOUBLE_KAJAK_NOT_AVAILABLE = "Doppelkajak nicht verfügbar";
+$ERROR_RESERVATION_KAJAK_TYPE_NOT_FOUND = "Kajak-Typ nicht gefunden";
 $ERROR_RESERVATION_KAJAK_NOT_AVAILABLE = "Kajaks nicht verfügbar";
 $ERROR_RESERVATION_KAJAK_NOT_SELECTED = "Bitte wähle einen Kajak aus.";
 $ERROR_RESERVATION = "Ein Fehler ist aufgetreten.";
 
 /* E-Mail */
 $ERROR_MAIL_NOT_SENT = "E-Mail konnte nicht versendet werden.";
+$ERROR_SUCCESS_BUT_MAIL_NOT_SENT = "Reservierung erfolgreich, aber " . $ERROR_MAIL_NOT_SENT;
 
 /* Cancellation */
 $INFO_CANCELLATION_CANCELED = "Reservierung wurde storniert.";
 $ERROR_CANCELLATION = "Reservierung konnte nicht storniert werden.";
 $ERROR_CANCELLATION_NOT_FOUND = "Reservierung wurde nicht gefunden.";
+
+/* Kajak */
+$ERROR_TYPE_NOT_IN_CONFIG = "Kajak-Typ nicht in Konfiguration gefunden";
