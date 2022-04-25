a47e37b6ca90b9343b7347e35642d85e656e5bd9
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-26 01:47:01 +0200

    page_user_privacy.php: moved text.

---
+++ pages/user/page_user_privacy.php
@@ -0,0 +1,136 @@
+<?php
+echo create_header('Datenschutz');
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col content">
+            <div class="content-wrapper">
+                <ol>
+                    <li>
+                        Ein Datenschutzbeauftragter ist erforderlich, da mehr als 9 Personen regelmäßig mit der
+                        Verarbeitung von Daten im Sinne von Art. 4 DSGVO befasst sind.
+                        <ol>
+                            <li>
+                                Verantwortlicher für die Datenverarbeitung ist: Die HTWG Konstanz,
+                                Alfred-Wachtelstraße 8, 78467
+                                Konstanz.
+                                Tel: +49 (0)711/906-0, E-Mail: <a href="mailto:">ToDo</a>
+                            </li>
+                            <li>
+                                <p>
+                                    Datenerfassung beim Besuch unsere Webseite:
+                                    <br>
+                                    Bei der bloßen informatorischen Nutzung unserer Webseite werden keine
+                                    personenbezogenen
+                                    Daten
+                                    erhoben.
+                                    Wir erheben nur solche Daten, die Ihr Browser an uns übermittelt
+                                    ("Server-Logfiles").
+                                    <br>
+                                    Bei der Nutzung unserer Webseite erheben wir folgende Daten:
+                                <ul>
+                                    <li>
+                                        Browsertyp und -version
+                                    </li>
+                                    <li>
+                                        Betriebssystem
+                                    </li>
+                                    <li>
+                                        IP-Adresse
+                                    </li>
+                                    <li>
+                                        Datum und Uhrzeit des Zugriffs
+                                    </li>
+                                    <li>
+                                        Seitenadressen
+                                    </li>
+                                    <li>
+                                        Zugriffszeit
+                                    </li>
+                                    <li>
+                                        Menge der gesendeten Daten
+                                    </li>
+                                </ul>
+                                Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. b DSGVO auf Basis unseres
+                                berechtigten
+                                Interesses
+                                an der Verbesserung der Stabilität und Funktionalität unserer Website.
+                                Eine Weitergabe oder anderweitige Verwendung der Daten erfolgt nicht. Wir behalten
+                                uns
+                                allerdings
+                                vor, die Server-Logfiles nachträglich zu überprüfen, sollten konkrete Anhaltspunkte
+                                auf
+                                eine
+                                rechtswidrige Nutzung hinweisen.
+                                <br><br>
+                                </p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        Cookies
+                        <p>
+                            Um den Besuch unserer Website attraktiv zu gestalten und die Nutzung bestimmter Funktionen
+                            zu
+                            ermöglichen, verwenden wir auf verschiedenen Seiten sogenannte Cookies.
+                            Hierbei handelt es sich um kleine Textdateien, die auf Ihrem Endgerät abgelegt werden.
+                            Einige der von uns verwendeten Cookies werden nach dem Ende der Browser-Sitzung, also nach
+                            Schließen
+                            Ihres Browsers, wieder gelöscht (sog. Sitzungs-Cookies).
+                            Andere Cookies verbleiben auf Ihrem Endgerät und ermöglichen uns oder unseren
+                            Partnerunternehmen
+                            (Cookies von Drittanbietern), Ihren Browser beim nächsten Besuch wiederzuerkennen
+                            (persistente
+                            Cookies).
+                            Werden Cookies gesetzt, erheben und verarbeiten diese im individuellen Umfang bestimmte
+                            Nutzerinformationen wie Browser- und Standortdaten sowie IP-Adresswerte. Persistente Cookies
+                            werden
+                            automatisiert nach einer vorgegebenen Dauer gelöscht, die sich je nach Cookie unterscheiden
+                            kann.
+                        </p>
+                        <p>
+                            Sofern durch einzelne von uns implementierte Cookies auch personenbezogene Daten verarbeitet
+                            werden,
+                            erfolgt die Verarbeitung gemäß Art. 6 Abs. 1 lit. f DSGVO zur Wahrung unserer berechtigten
+                            Interessen an
+                            der bestmöglichen Funktionalität der Website sowie einer kundenfreundlichen und effektiven
+                            Ausgestaltung
+                            des Seitenbesuchs.
+
+                            Bitte beachten Sie, dass Sie Ihren Browser so einstellen können, dass Sie über das Setzen
+                            von
+                            Cookies
+                            informiert werden und einzeln über deren Annahme entscheiden oder die Annahme von Cookies
+                            für
+                            bestimmte
+                            Fälle oder generell ausschließen können.
+                            Jeder Browser unterscheidet sich in der Art, wie er die Cookie-Einstellungen verwaltet.
+                            Diese ist in dem Hilfemenü jedes Browsers beschrieben, welches Ihnen erläutert, wie Sie Ihre
+                            Cookie-Einstellungen ändern können.
+                        </p>
+                    </li>
+                    <li>
+                        Rechte des Betroffenen
+                        <p>
+                            Widerspruchsrecht:
+                        </p>
+                    </li>
+                    <li>
+                        Dauer der Speicherung personenbezogener Daten
+                        <p>
+                            Die Dauer der Speicherung von personenbezogenen Daten bemisst sich anhand der jeweiligen
+                            gesetzlichen
+                            Aufbewahrungsfrist (z.B. handels- und streurrechtliche Aufbewahrungsfristen).
+                            Nachablauf der Frist werden die entsprechenden Daten routinemäßig gelöscht, sofern sie nicht
+                            mehr
+                            zur
+                            Vertragserfüllung oder Vertragsanbahung erforderlich sind und/oder unsererseits kein
+                            berechtigtes
+                            Interesse an der Weiterspreicherung fortbesteht.
+                        </p>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
