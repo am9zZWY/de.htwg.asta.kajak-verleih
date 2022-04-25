7fe08549dd2d9fc5fb8ea4247c6fa5ccbc7c6612
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-26 01:46:41 +0200

    :lipstick: page_user_impressum.php: changed layout.

--- pages/user/page_user_impressum.php
+++ pages/user/page_user_impressum.php
@@ -1,42 +1,54 @@
 <?php
 echo create_header('Impressum')
 ?>
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="content">
-            <div>
+        <div class="col content">
+            <div class="content-wrapper">
                 <h3>AStA</h3>
-                <strong>Hochschule Konstanz</strong>
-                <br>
-                Alfred-Wachtelstraße 8 <br>
-                78462 Konstanz<br>
-                Gebäude D<br><br><br>
-                <strong>Kontaktaufnahme</strong><br>
-                Tel.: 07531 206-431/-122<br>
-                E-Mail: asta@htwg-konstanz.de
+                <p>
+                    <strong>Hochschule Konstanz</strong>
+                </p>
+                <p>
+                    Alfred-Wachtel-Straße 8 <br>
+                    78462 Konstanz<br>
+                    Gebäude D<br><br><br>
+                    <strong>Kontaktaufnahme</strong><br>
+                    Tel.: 07531 206-431/-122<br>
+                    E-Mail: asta-kajak (at) htwg-konstanz.de
+                </p>
             </div>
-            <div class="row" style="text-align: justify;">
-                <button class="accordion"><strong>Urheberrecht</strong></button>
-                <div class="panel">
-                    <p>
-                    <strong>Die auf unseren Internetseiten enthaltene Werke und INhalte unterstehen dem Urheberrecht.</strong><br>
-                    <br>
-                        Ohne schriftliche Genehminung des jeweiligen Ersteller oder Autors dürfen die Werke bzw. Inhalte weder vervielfältigt, bearbeitet, noch verbreitet werden.
-                        Das Herunterladen und Kopieren unseres Angebots ist ausschließlich für den privaten, nicht kommerziellen Gebrauch erlaubt.
-                    <br>
-                    </p>
-                </div>
-                <button class="accordion"><strong>Haftung für Inhalte</strong></button>
-                <div class="panel">
-                    <p>
-                        <strong>Sämtliche Inhalte unserer Internetseite sind mit größtmöglicher Sorgfalt erstellt worden.</strong>
-                        Dennoch können wir keine Gewähr für deren Richtigkeit, Vollständigkeit und Aktualität übernehmen.
-                        Unsere Haftung beginnt jedoch erst in dem Moment, in welchem wir von einer konkreten Rechtsverletzung Kenntis erlangen.
-                        Bei entsprechender Kenntnis werden wir die ebtroffen Inhalte unverzüglich entfernen.
-
-                    </p>
-                </div>
-                <?php echo create_accordion() ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col content">
+            <div class="content-wrapper">
+                <h4>Urheberrecht</h4>
+                <p>
+                    <strong>Die auf unseren Internetseiten enthaltene Werke und Inhalte unterstehen dem
+                        Urheberrecht.</strong>
+                </p>
+                <p>
+                    Ohne schriftliche Genehmigung des jeweiligen Erstellers oder Autors dürfen die Werke bzw. Inhalte
+                    weder vervielfältigt, bearbeitet, noch verbreitet werden.
+                    Das Herunterladen und Kopieren unseres Angebots ist ausschließlich für den privaten, nicht
+                    kommerziellen Gebrauch erlaubt.
+                </p>
+            </div>
+        </div>
+        <div class="col content">
+            <div class="content-wrapper">
+                <h4>Haftung für Inhalte</h4>
+                <p>
+                    <strong>Sämtliche Inhalte unserer Internetseite sind mit größtmöglicher Sorgfalt erstellt
+                        worden.</strong>
+                </p>
+                <p>
+                    Dennoch können wir keine Gewähr für deren Richtigkeit, Vollständigkeit und Aktualität übernehmen.
+                    Unsere Haftung beginnt jedoch erst in dem Moment, in welchem wir von einer konkreten
+                    Rechtsverletzung Kenntnis erlangen.
+                    Bei entsprechender Kenntnis werden wir die betroffenen Inhalte unverzüglich entfernen.
+                </p>
             </div>
         </div>
     </div>
