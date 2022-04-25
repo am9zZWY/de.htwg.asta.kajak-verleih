79fd44b1eea1b9b2fb61b71371c8a3de6e262406
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-26 01:46:03 +0200

    :lipstick: page_user_agb.php: changed layout.

--- pages/user/page_user_agb.php
+++ pages/user/page_user_agb.php
@@ -2,53 +2,74 @@
 echo create_header('Informationen zur Reservierung');
 ?>
 <div class="container-fluid">
-    <div class="container">
-        <div class="row">
-            <div class="content">
-                <div>
-                    <h3>Mietsachen</h3>
-                    <p>Vermietet werden Kajaks, mit einem Mietvertrag festgehalten Ausrüstungsgegenstände.
-                        Diese werden für einen sicheren Betrieb der Kajaks benötigt.
-                        Der Mieter verpflichtet sich, die Mietgegenstände mit Sorgfalt zu behandeln. Schäden und
-                        Verschmutzungen werden auf Kosten des Mieters entfernt. Zum festgelegten Abhol-/Rückholtermin
-                        hat der Mieter die Mietgegenstände vollständig und persönlich dem Vermieter zurückzugeben. Fehlende
-                        oder kaputte Gegenstände werden zum Wiederbeschaffungswert berechnet.
-                    </p>
-                </div>
+    <div class="row content">
+        <div class="content-wrapper">
+            <div>
+                <h3>Mietsachen</h3>
+                <p>Vermietet werden Kajaks, mit einem Mietvertrag festgehalten Ausrüstungsgegenstände.
+                    Diese werden für einen sicheren Betrieb der Kajaks benötigt.
+                    Der Mieter verpflichtet sich, die Mietgegenstände mit Sorgfalt zu behandeln. Schäden und
+                    Verschmutzungen werden auf Kosten des Mieters entfernt. Zum festgelegten Abhol-/Rückholtermin
+                    hat der Mieter die Mietgegenstände vollständig und persönlich dem Vermieter zurückzugeben.
+                    Fehlende
+                    oder kaputte Gegenstände werden zum Wiederbeschaffungswert berechnet.
+                </p>
             </div>
         </div>
-        <div class="row" style="text-align: justify;">
-            <button class="accordion"><strong>Rechtliches</strong></button>
-            <div class="panel">
+    </div>
+    <div class="row">
+        <div class="col content">
+            <div class="content-wrapper">
+                <h4>Rechtliches</h4>
                 <p>test ipsum..</p>
             </div>
-            <button class="accordion"><strong>Sicherheit</strong></button>
-            <div class="panel">
-                <p><strong>Jeder in einem Kajak Fahrende muss schwimmen können und eine Schwimmweste tragen.</strong>
-                    <br><br>Aus Sicherheitsgründen dürfen Kinder mit uns nur <strong>ab dem 12. Lebensjahr</strong> an
-                    Kajakfahrten teilnehmen.
+        </div>
+        <div class="col content">
+            <div class="content-wrapper">
+                <h4>Sicherheit</h4>
+                <p><strong>Jeder in einem Kajak Fahrende muss schwimmen können und eine Schwimmweste
+                        tragen.</strong>
+                </p>
+                <p>
+                    Aus Sicherheitsgründen dürfen Kinder mit uns nur <strong>ab dem 12. Lebensjahr</strong>
+                    an Kajakfahrten teilnehmen.
                     Die erwachsenen Aufsichtspersonen tragen die volle Verantwortung.
                     Eigene Westen sind unerwünscht. Aufblasbare Schwimmhilfen sind nicht erlaubt.
                     Bei Missachtung dieser Vorsichtsmaßnahmen sind wir berechtigt, die Tour sofort abzubrechen.
                 </p>
             </div>
-            <button class="accordion"><strong>Höhere Gewalt</strong></button>
-            <div class="panel">
-                <p><strong>Der AStA der HTWG</strong> ist bei einer Gesundheit gefährdenden Situation aufgrund der
+        </div>
+    </div>
+    <div class="row">
+        <div class="col content">
+            <div class="content-wrapper">
+                <h4>Höhere Gewalt</h4>
+                <p>
+                    <strong>Der AStA der HTWG</strong> ist bei einer Gesundheit gefährdenden Situation aufgrund der
                     Wetterlage
-                    (Sturm, Unwetter, Überschwemmungen, extremes Niedrigwasser oder starker Nebel) berechtigt, <strong>Touren
+                    (Sturm, Unwetter, Überschwemmungen, extremes Niedrigwasser oder starker Nebel) berechtigt,
+                    <strong>Touren
                         abzusagen.</strong>
-                    <br><br>In diesem Falle hat der Mieter <strong>keinen Anspruch</strong> auf Kostenerstattung von
+                </p>
+                <p>
+                    In diesem Falle hat der Mieter <strong>keinen Anspruch</strong> auf Kostenerstattung von
                     Nebenkosten
                     (Anreise, Hotelübernachtung etc.).
-                    Dieses gilt auch für den Fall, dass die Tour aufgrund niedriger Wasserstände nicht stattfindet oder
-                    auf ein anderes Gewässer verlegt werden muss.</p>
+                    Dieses gilt auch für den Fall, dass die Tour aufgrund niedriger Wasserstände nicht stattfindet
+                    oder
+                    auf ein anderes Gewässer verlegt werden muss.
+                </p>
             </div>
-            <button class="accordion"><strong>Sorgfaltspflicht</strong></button>
-            <div class="panel">
-                <p><strong>Fahrverbote und private Gebote in Bereichen nicht öffentlicher Grundstücke sind
+        </div>
+        <div class="col content">
+            <div class="content-wrapper">
+                <h4>Sorgfaltspflicht</h4>
+                <p>
+                    <strong>Fahrverbote und private Gebote in Bereichen nicht öffentlicher Grundstücke sind
                         einzuhalten.</strong>
-                    <br><br>Wiesen, Weiden und Felder an den Ufern sind in privatem Besitz und dürfen nicht betreten
+                </p>
+                <p>
+                    Wiesen, Weiden und Felder an den Ufern sind in privatem Besitz und dürfen nicht betreten
                     oder zur Rast genutzt werden.
                     <strong>Während der Fahrt dürfen die Kajaks nur an öffentlichen und besonders gekennzeichneten
                         Rastplätzen verlassen werden.</strong>
@@ -58,59 +79,9 @@ echo create_header('Informationen zur Reservierung');
                     Bei Missachtung der Natur, grobem Fehlverhalten – auch durch Alkoholmissbrauch -, Lärm,
                     Materialmissbrauch und unzulässiger Müllentsorgung kann der Vermieter den sofortigen Abbruch der
                     Tour fordern.
-                    Sämtliche Kosten und Folgekosten sind vom Auftragsgeber zu zahlen.</div>
-            <button class="accordion"><strong>Datenschutz</strong></button>
-            <div class="panel">
-                <p>
-                    <strong>Ein Datenschutzbeauftragter ist erforderlich, da mehr als 9 Personen regelmäßig mit der
-                        Verarbeitung von Daten im Sinne von Art. 4 DSGVO befasst sind.
-                    </strong><br>
-                    <br>
-                    1.1 Verantwortlicher für die Datenverarbeitung ist: Die HTWG Konstanz, Alfred-Wachtelstraße 8, 78467 Konstanz.
-                    Tel: +49 (0)711/906-0, E-Mail: <a href="mailto:">ToDo</a>
-                    <br><br>
-                    1.2 Datenerfassung beim Besuch unsere Webseite:
-                    <br>
-                    Bei der bloßen informatorischen Nutzung unserer Webseite werden keine personenbezogenen Daten erhoben.
-                    Wir erheben nur solche Daten, die Ihr Browser an uns übermittelt ("Server-Logfiles").
-                    <br>
-                    Bei der Nutzung unserer Webseite erheben wir folgende Daten:
-                    - Browsertyp und -version
-                    - Betriebssystem
-                    - Ip-Adresse
-                    - Datum und Uhrzeit des Zugriffs
-                    - Seitenadressen
-                    - Zugriffszeit
-                    - Menge der gesendeten Daten
-                    <br><br>
-                    Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. b DSGVO auf Basis unseres berechtigten Interesses an der Verbesserung der Stabilität und Funktionalität unserer Website.
-                    Eine Weitergabe oder anderweitige Verwendung der Daten erfolgt nicht. Wir behalten uns allerdings vor, die Server-Logfiles nachträglich zu überprüfen, sollten konkrete Anhaltspunkte auf eine rechtswidrige Nutzung hinweisen.
-                    <br><br>
-
-                    <strong><h4>2. Cookies</h4></strong>
-                Um den Besuch unserer Website attraktiv zu gestalten und die Nutzung bestimmter Funktionen zu ermöglichen, verwenden wir auf verschiedenen Seiten sogenannte Cookies.
-                Hierbei handelt es sich um kleine Textdateien, die auf Ihrem Endgerät abgelegt werden.
-                Einige der von uns verwendeten Cookies werden nach dem Ende der Browser-Sitzung, also nach Schließen Ihres Browsers, wieder gelöscht (sog. Sitzungs-Cookies).
-                Andere Cookies verbleiben auf Ihrem Endgerät und ermöglichen uns oder unseren Partnerunternehmen (Cookies von Drittanbietern), Ihren Browser beim nächsten Besuch wiederzuerkennen (persistente Cookies).
-                Werden Cookies gesetzt, erheben und verarbeiten diese im individuellen Umfang bestimmte Nutzerinformationen wie Browser- und Standortdaten sowie IP-Adresswerte. Persistente Cookies werden automatisiert nach einer vorgegebenen Dauer gelöscht, die sich je nach Cookie unterscheiden kann.
-                <br><br>
-                Sofern durch einzelne von uns implementierte Cookies auch personenbezogene Daten verarbeitet werden, erfolgt die Verarbeitung gemäß Art. 6 Abs. 1 lit. f DSGVO zur Wahrung unserer berechtigten Interessen an der bestmöglichen Funktionalität der Website sowie einer kundenfreundlichen und effektiven Ausgestaltung des Seitenbesuchs.
-
-                Bitte beachten Sie, dass Sie Ihren Browser so einstellen können, dass Sie über das Setzen von Cookies informiert werden und einzeln über deren Annahme entscheiden oder die Annahme von Cookies für bestimmte Fälle oder generell ausschließen können.
-                Jeder Browser unterscheidet sich in der Art, wie er die Cookie-Einstellungen verwaltet.
-                Diese ist in dem Hilfemenü jedes Browsers beschrieben, welches Ihnen erläutert, wie Sie Ihre Cookie-Einstellungen ändern können.
-                <br><br>
-                <strong><h4>Rechte des Betroffenen</h4></strong>
-                ..
-                Widerspruchsrecht:
-                <br><br>
-                <strong><h4>Dauer der Speicherung personenbezogener Daten</h4></strong>
-                Die Dauer der Speicherung von personenbezogenen Daten bemisst sich anhand der jeweiligen gesetzlichen Aufbewahrungsfrist (z.B. handels- und streurrechtliche Aufbewahrungsfristen).
-                Nachablauf der Frist werden die entsprechenden Daten routinemäßig gelöscht, sofern sie nicht mehr zur Vertragserfüllung oder Vertragsanbahung erforderlich sind und/oder unsererseits kein berechtigtes Interesse an der Weiterspreicherung fortbesteht.
-                <br><br>
+                    Sämtliche Kosten und Folgekosten sind vom Auftragsgeber zu zahlen.
                 </p>
             </div>
-            <?php echo create_accordion() ?>
         </div>
     </div>
 </div>
