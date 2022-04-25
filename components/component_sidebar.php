48b6b04cf20358decaaf86d3440322728bf551c1
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-26 01:43:57 +0200

    component_sidebar.php: more like a nav now.

--- components/component_sidebar.php
+++ components/component_sidebar.php
@@ -10,28 +10,56 @@ if (($_SERVER['REQUEST_METHOD'] === 'POST') && isset($_POST['logout'])) {
 }
 ?>
 
-<nav class="fixed-top">
-    <div id="burger" class="sidenav-burger">
-        <span onclick="openNav()">&#9776;</span>
+<nav class="navbar navbar-expand-lg bg-light fixed-top">
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="sidenav-burger" id="burger">
+                <span onclick="openNav()">&#9776;</span>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/privacy">
+                    Datenschutz
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/impressum">
+                    Impressum
+                </a>
+            </li>
+        </ul>
     </div>
 </nav>
 <div id="sidenav" class="sidenav">
-    <a href="javascript:void(0)" class="close-btn" onclick="closeNav()">&times;</a>
-    <a href="/">Startseite</a>
-    <a href="/about">Info</a>
-    <a href="/impressum">Impressum</a>
-    <?php
-    if (is_logged_in()) {
-        ?>
-        <a href="/admin">Dashboard</a>
-        <a href="/config">Konfiguration</a>
-        <a href="/how_to_admin">Anleitung</a>
-        <form method="POST">
-            <button type="submit" name="logout">Abmelden</button>
-        </form>
+    <ul class="nav nav-flush flex-column mb-auto">
+        <li class="navbar-toggler">
+            <a href="javascript:void(0)" class="close-btn" onclick="closeNav()">&times;</a>
+        </li>
+        <li>
+            <a class="nav-link" href="/">Startseite</a>
+        </li>
+        <li>
+            <a class="nav-link" href="/about">Info</a>
+        </li>
         <?php
-    }
-    ?>
+        if (is_logged_in()) {
+            ?>
+            <li>
+                <a class="nav-link" href="/admin">Dashboard</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/config">Konfiguration</a>
+            </li>
+            <li>
+                <form method="POST">
+                    <button type="submit" name="logout">Abmelden</button>
+                </form>
+            </li>
+            <?php
+        }
+        ?>
+    </ul>
 </div>
 <script lang="javascript">
     function openNav() {
