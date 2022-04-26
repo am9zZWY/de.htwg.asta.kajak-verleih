f5c3251c6bb55e258b15a612e2f2fe888ad2a1ef
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-26 23:12:33 +0200

    :recycle: page_admin_config.php: redid code.

--- pages/admin/page_admin_config.php
+++ pages/admin/page_admin_config.php
@@ -7,26 +7,68 @@ global $config;
         <div class="content-wrapper">
             <p>Aktuelle Werte:</p>
             <ul>
-                <?php
-                echo "<li><strong>" . "Preise:" . "</strong><br>";
-                foreach ($config->getPrices(true) as $price) {
-                    echo $price->description . ': ' . $price->value . ' €' . '<br>';
-                }
-                echo '</li><br>';
+                <!-- PRICES -->
+                <li>
+                    <strong>Preise:</strong>
+                    <ul>
+                        <?php
+                        foreach ($config->getPrices(true) as $price) {
+                            ?>
+                            <li>
+                                <?php echo $price->description ?>: <?php echo $price->value ?>
+                            </li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                </li>
 
-                foreach ($config->getKajaks(true) as $kajak) {
-                    echo '<li>' . "<strong>" . $kajak->name . ': ' . "</strong>" . "<br>" . "Sitze: " . $kajak->seats . "<br>" . "Anzahl: " . $kajak->amount . '</li>';
-                }
-                echo '<br>';
+                <!-- KAJAKS -->
+                <li>
+                    <strong>Kajaks:</strong>
+                    <ul>
+                        <?php
+                        foreach ($config->getKajaks(true) as $kajak) {
+                            ?>
+                            <li>
+                                <?php echo $kajak->name ?>:
+                                <ul>
+                                    <li>
+                                        Sitze: <?php echo $kajak->seats ?>
+                                    </li>
+                                    <li>
+                                        Anzahl: <?php echo $kajak->amount ?>
+                                    </li>
+                                </ul>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </li>
 
-                foreach ($config->getTimeslots() as $timeslot) {
-                    echo '<li><strong>' . $timeslot->name . "</strong>" . ': ' . $config->formatTimeslot($timeslot) . '</li>';
-                }
+                <!-- TIMESLOTS -->
+                <li>
+                    <strong>Timeslots:</strong>
+                    <ul>
+                        <?php
+                        foreach ($config->getTimeslots() as $timeslot) {
+                            ?>
+                            <li>
+                                <?php echo $timeslot->name ?>: <?php echo $config->formatTimeslot($timeslot) ?>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </li>
 
-                echo '<br>';
-                $days = $config->getDays();
-                echo "<li><strong>" . "Reservierungs-Zeitraum: " . "</strong><br>" . $days->min_days . ' - ' . $days->max_days . ' Tage' . "</li>";
-                ?>
+                <!-- UNSORTIERT -->
+                <li>
+                    <strong>Unsortiert:</strong>
+                    <ul>
+                        <li>
+                            <?php $days = $config->getDays(); ?>
+                            Reservierungszeitraum: <?php echo $days->min_days . ' - ' . $days->max_days ?> Tage
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
