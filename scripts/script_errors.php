5cd9859678e4424c2c28a302540c0a60c81a54fd
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-29 20:24:17 +0200

    script_errors.php: added more infos.

--- scripts/script_errors.php
+++ scripts/script_errors.php
@@ -3,14 +3,19 @@
 
 /* Database */
 $ERROR_DATABASE_CONNECTION = "Konnte keine Verbindung zur Datenbank herstellen. Läuft diese überhaupt?";
-$ERROR_DATABASE_QUERY = "Fehler bei der Vorbereitung";
+$ERROR_DATABASE_QUERY = "Fehler bei der Vorbereitung.";
+
+/* Table */
+$INFO_TABLE_CREATED = "Eine Tabelle wurde erfolgreich erstellt.";
+$ERROR_TABLE_CREATION = "Eine Tabelle konnte nicht erstellt werden.";
 
 /* Form reservation */
 $ERROR_RESERVATION_TIMESLOT_NOT_SELECTED = "Bitte wähle eine Zeit aus.";
-$ERROR_RESERVATION_KAJAK_TYPE_NOT_FOUND = "Kajak-Typ nicht gefunden";
-$ERROR_RESERVATION_KAJAK_NOT_AVAILABLE = "Kajaks nicht verfügbar";
+$ERROR_RESERVATION_KAJAK_TYPE_NOT_FOUND = "Kajak-Typ nicht gefunden.";
+$ERROR_RESERVATION_KAJAK_NOT_AVAILABLE = "Kajaks nicht verfügbar.";
 $ERROR_RESERVATION_KAJAK_NOT_SELECTED = "Bitte wähle einen Kajak aus.";
 $ERROR_RESERVATION = "Ein Fehler ist aufgetreten.";
+$INFO_RESERVATION_SUCCESS = "Reservierung erfolgreich!";
 
 /* E-Mail */
 $ERROR_MAIL_NOT_SENT = "E-Mail konnte nicht versendet werden.";
@@ -19,10 +24,11 @@ $ERROR_SUCCESS_BUT_MAIL_NOT_SENT = "Reservierung erfolgreich, aber " . $ERROR_MA
 /* Cancellation */
 $INFO_CANCELLATION_CANCELED = "Reservierung wurde storniert.";
 $ERROR_CANCELLATION = "Reservierung konnte nicht storniert werden.";
-$ERROR_CANCELLATION_NOT_FOUND = "Reservierung wurde nicht gefunden.";
+$ERROR_CANCELLATION_NOT_FOUND = "Reservierung konnte gefunden nicht gefunden.";
 
 /* Kajak */
-$ERROR_TYPE_NOT_IN_CONFIG = "Kajak-Typ nicht in Konfiguration gefunden";
+$ERROR_TYPE_NOT_IN_CONFIG = "Kajak-Typ nicht in Konfiguration gefunden.";
+$ERROR_KAJAK_NOT_CREATED = "Kajak konnte nicht erstellt werden.";
 
 /* Login */
 $ERROR_LOGIN = "Einloggen fehlgeschlagen!";
